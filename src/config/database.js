0f9d3dde4d3e80e08a711351c93a1f0577e136e3
Álex Porto <alexportoferreira@hotmail.com>
2019-04-24 12:31:20 -0300

    Add and config sequelize and PostgreSQL

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
