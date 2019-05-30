edbcb2d0c22c3eae34e40ed414d3672e0e2e315a
Álex Porto <alexportoferreira@hotmail.com>
2019-05-30 13:34:26 -0300

    Add authentication,logout and session

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.render('auth/singup')
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
+routes.use('/app', authMiddleware)
+
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', guestMiddleware, SessionController.create)
+routes.get('/signin', SessionController.store)
+
+routes.get('/signup', guestMiddleware, UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/logout', SessionController.destroy)
+
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  return res.render('dashboard')
 })
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const session = require('express-session')
 const nunjucks = require('nunjucks')
 const path = require('path')
+const LokiStore = require('connect-loki')(session)
 
 class App {
   constructor () {
@@ -14,6 +16,16 @@ class App {
 
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(
+      session({
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        secret: 'MyAppSecret',
+        resave: false,
+        saveUninitialized: true
+      })
+    )
   }
   views () {
     nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
